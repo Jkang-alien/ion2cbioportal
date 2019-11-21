a16b0dca0084c9d6742a8f13aec318fbfefdee3f
molpath <jkang.alien@gmail.com>
2019-11-22 08:28:08 +0900

    cnv.R

---
+++ cnv.R
@@ -0,0 +1,23 @@
+library(cnv4cbio)
+setwd('/home/molpath/cbioportal_data_file/20181109/TSV/')
+
+f_list <- list.files('.')
+
+table <- as.numeric(entrez_id)
+for (j in f_list) {
+  table <- cbind(table, cnv_table(j))
+}
+
+colnames(table)[1] <- 'Entrez_Gene_Id'
+
+discrete_table <- table
+
+discrete_table[is.na(discrete_table)] <- 0
+discrete_table[discrete_table>=2] <- 2
+discrete_table[,1] <- as.numeric(entrez_id)
+
+write.table(table, 'data_log2_CNA.txt', append = FALSE, quote = FALSE,
+            sep = "\t", na = "NA", dec = ".", col.names = TRUE, row.names = FALSE)
+
+write.table(discrete_table, '/home/molpath/cbioportal_data_file/data_CNA.txt', append = FALSE, quote = FALSE,
+            sep = "\t", na = "NA", dec = ".", col.names = TRUE, row.names = FALSE)
